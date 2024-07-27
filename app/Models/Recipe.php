19920c446bcbe2fae5192992eab26e369fe7aa4b
Samms206 <sh2069936@gmail.com>
2024-07-27 22:38:26 +0700

    updated all model

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Recipe extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'thumbnail',
+        'about',
+        'url_file',
+        'url_video',
+        'category_id',
+        'recipe_author_id'
+
+    ];
 }
